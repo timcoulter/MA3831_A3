473f5bd883cb19a977396cda9ba381e54a39ac25
timcoulter <36687954+timcoulter@users.noreply.github.com>
2021-05-24 12:18:03 +1000

    Increased robustness of processing

--- preprocess_data.py
+++ preprocess_data.py
@@ -12,6 +12,7 @@ except Exception:
 pd.set_option('mode.chained_assignment', None)
     
 df = pickle.load(open("movie_data2.p","rb"))
+print(df.head())
 
 #First fix the date and country
 
@@ -36,40 +37,47 @@ columns_review = ['title','votes','rating','length','genre','country','date','re
 title = list(df['title'])
 
 #lemmatize sentences and paragraphs for reviews and plots
-plot_df = pd.DataFrame(columns=columns_plot)
-review_df = pd.DataFrame(columns=columns_review)
-
+try:
+    plot_df = pickle.load(open("plot_df.p","rb"))
+except Exception:
+    plot_df = pd.DataFrame(columns=columns_plot)
+    
+try:
+    review_df = pickle.load(open("review_df.p","rb"))
+except Exception:
+    review_df = pd.DataFrame(columns=columns_review)
 
 for i in tqdm.tqdm(range(len(title))):
     
+    processed_titles = list(plot_df['title'])
+    if title[i] in processed_titles:
+        continue
+
     X = df.loc[df['title'] == title[i]]
     X = X.reset_index()
     X = X.head(1)
     p = X.loc[0,'plot']
     r = X.loc[0,'review']
     
+    p = p.replace('...','.').split('<>')
+    r = r.replace('...','.').split('<>')
+    
     for i in range(len(p)):
-        try:
-            p2 = p[i].split('. ')
-            p2 = ' '.join([clean_words(x) for x in p])
-            X2 = X[columns_plot]
-            X2.loc[0,'plot'] = p2
-            plot_df = pd.concat([plot_df,X2])
-        except Exception:
-            print(title[i])
+        p2 = p[i].split('. ')
+        p2 = ' '.join([clean_words(x) for x in p])
+        X2 = X[columns_plot]
+        X2.loc[0,'plot'] = p2
+        plot_df = pd.concat([plot_df,X2])
 
     for i in range(len(r)):
-        try:
-            r2 = r[i].split('. ')
-            r2 = ' '.join([clean_words(x) for x in r])
-            X2 = X[columns_review]
-            X2.loc[0,'review'] = r2
-            review_df = pd.concat([review_df,X2])
-        except Exception:
-            print(title[i])
+        r2 = r[i].split('. ')
+        r2 = ' '.join([clean_words(x) for x in r])
+        X2 = X[columns_review]
+        X2.loc[0,'review'] = r2
+        review_df = pd.concat([review_df,X2])
         
-pickle.dump(plot_df,open('plot_df.p', "wb" ))
-pickle.dump(review_df,open('review_df.p', "wb" ))
+    pickle.dump(plot_df,open('plot_df.p', "wb" ))
+    pickle.dump(review_df,open('review_df.p', "wb" ))
         
         
 
